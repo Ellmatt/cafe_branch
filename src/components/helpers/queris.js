a396ad91dfadede5841760804de3f2a29e008ed2
matias <pagosptcmatias@gmail.com>
2022-10-11 18:40:54 -0300

    creamos card crearproducto

--- src/components/helpers/queris.js
+++ src/components/helpers/queris.js
@@ -8,17 +8,36 @@ const URL = "http://localhost:3004/productos";
 // peticion DELETE, peticion para borrar
 // peticion PUT, peticon
 
-
-export const  consultarApi = async () => {
+export const consultarApi = async () => {
   try {
     // fetch para peticiones
     // await para esperar
-    const respuesta = await fetch(URL)
+    const respuesta = await fetch(URL);
     // .json extrae datos en la propieda de la respuesta
-    const listaProductos= await respuesta.json()
+    const listaProductos = await respuesta.json();
     // console.log(respuesta)
     return listaProductos;
-   
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// peticion POST
+export const crearProductoAPI = async () => {
+  try {
+    // fetch para peticiones
+    // await para esperar
+    const respuesta = await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(producto),
+    });
+    // .json extrae datos en la propieda de la respuesta
+    const listaProductos = await respuesta.json();
+    // console.log(respuesta)
+    return listaProductos;
   } catch (error) {
     console.log(error);
   }
